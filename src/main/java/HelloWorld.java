58e27a7cbc4bc56213822cd5d1744b0983685a31
Naeem2022 <link2naeem@gmail.com>
2022-04-17 18:09:32 +0100

    First commit to copy

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,15 @@
+import com.test.Class1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		Class1 cl1=new Class1();
+		cl1.showMessage();
+		//  adding comment
+		System.out.println("This is my practic session");
+		
+		System.out.println("I  am commiting 3rd time to repository");
+	}
+
+}
---
+++ src/main/java/com/test/Class1.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class Class1 {
+	public void showMessage() {
+		System.out.println(" This is Class 1");
+	}
+
+}
